4f14ea96647783b2ceb41462217f6d21d022b4e7
Martin Feder <martinf@migal.org.il>
2022-05-17 18:21:10 +0300

    mismatched indices for run\predictions fix

--- GWANN.py
+++ GWANN.py
@@ -128,11 +128,13 @@ def run(vcf,pheno_path,trait,model,output_path):
 
     pheno = pd.read_csv(pheno_path,index_col=None,sep=',')
 
-    _,index_samples,_ = np.intersect1d(vcf_samples,pheno["sample"],return_indices=True)
+    _,index_samples,index_samples_pheno = np.intersect1d(vcf_samples,pheno["sample"],return_indices=True)
 
     # df_ss = pd.DataFrame(ss,columns=['sample'])
     # pheno = pd.concat([df_ss,pheno],axis=0)
     final_vcf = final_vcf[:,index_samples]
+    pheno = pheno.loc[index_samples_pheno].reset_index()
+    #assert (pheno["sample"] == vcf_samples[index_samples]).all()
 
     pheno_sorted = pheno.sort_values(by=[trait,"sample"],na_position='first')
 
