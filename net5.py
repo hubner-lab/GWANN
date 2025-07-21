76f243b4602357120c2c9ff948c7ecf590df3eaa
Amir Ammar <https://github.com/AmirAmmar123>
2025-07-21 10:11:06 +0300

    s

---
+++ net5.py
@@ -0,0 +1,36 @@
+from keras import layers, Input, Model
+import tensorflow as tf
+
+class ModelBuilder:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def build(self):
+        matrix_input = Input(shape=(self.height, self.width, 1))
+        
+        x = layers.Lambda(lambda x: x + 1.0)(matrix_input)
+
+        x = layers.Masking(mask_value=0)(x)
+        
+        x = layers.Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+        
+        x = layers.Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+        
+        x = layers.GlobalAveragePooling2D()(x)
+        
+        x = layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01))(x)
+        x = layers.Dropout(rate=0.5)(x)
+        
+        output = layers.Dense(2, activation='softmax')(x)
+
+        return Model(inputs=matrix_input, outputs=output)
+
+    def model_summary(self):
+        model = self.build()
+        model.summary()
+        return model
